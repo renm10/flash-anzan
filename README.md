07160120ba1ed7a9b193bd49b218372a731ff592
renm10 <43124584+renm10@users.noreply.github.com>
2023-06-13 02:19:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# フラッシュ暗算アプリ
+## TO DO LIST:
+- [ ] Add user Input of level and difficulty (Maybe ask how many numbers and how many seconds??)
+- [ ] Style Everything 
---
+++ src/script.js
@@ -0,0 +1,87 @@
+let min = 1; //Minimum
+let max = 99; //Maximum
+const countDownsec = 3; //Seconds it waits before starting
+let cdcounter = countDownsec; //Counter that counts the wait time
+let i = 0; //counter to go through the list of numbers
+let numberList; //List of Random numbers
+let acttotal; //Total of the list of numbers
+const flashElm = document.getElementById('number-label');
+let flashinterval = null; //interval for flash action
+let cdinterval = null; //interval for countdown
+
+function startGame() {
+    if (flashinterval)
+    {
+        clearInterval();
+        return;
+    }
+    //Create random number list
+    numberList = createRandList();
+    acttotal = calcTotal(numberList);
+    alert(acttotal);
+    alert("Hit OK If you are Ready!!");
+    cdinterval = setInterval(countDown, 1000);
+    runFlash();
+}
+
+function flashInterval() {
+    flashinterval = setInterval(updateNumber, 500, numberList);
+}
+
+function runFlash() {
+    setTimeout(flashInterval, 4000);
+}
+
+function countDown() {
+    if (cdcounter == 0) //Stop at 0 sec
+    {
+        clearInterval(cdinterval);
+        cdinterval = null;
+        cdcounter = 3;
+    }
+    else
+    {
+        flashElm.innerHTML = 'あと' + `${cdcounter}` + '秒';
+        cdcounter--;
+    }
+}
+
+function createRand() {
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    return random;
+}
+
+function createRandList()
+{
+    var numList = [];
+    for (let i = 0; i < 10; i++)
+    {
+        num = createRand();
+        numList.push(num);
+    }
+    return numList;
+}
+
+function calcTotal(list) {
+    var total = 0;
+    for (let i = 0; i < 10; i++)
+    {
+        total += list[i];
+    }
+    return total;
+}
+
+function updateNumber(list) {
+    if (i == 10) //Stop at the 10th element
+    {
+        clearInterval(flashinterval);
+        flashinterval = null;
+        i = 0;
+        flashElm.innerHTML = ``;
+    }
+    else
+    {
+        flashElm.innerHTML = `${list[i]}`;
+        i++;
+    }
+}
